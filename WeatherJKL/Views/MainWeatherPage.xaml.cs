54e0b20dd07ad1e5ecdf909236457090de361a5a
Umps <karl.pongolini@gmail.com>
2023-04-17 15:51:44 +0300

    Add MainWeatherPage

---
+++ WeatherJKL/Views/MainWeatherPage.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.Maui.Controls;
+namespace Views;
+
+
+
+public partial class MainWeaetherPage : ContentPage
+{
+    public MainWeaetherPage()
+    {
+        InitializeComponent();
+    }
+
+    //Redirects to different page
+
+    //private void OnNextPageButtonClicked(object sender, EventArgs e)
+    //{
+    //    Navigation.PushAsync(new NextPage());
+    //}
+}
