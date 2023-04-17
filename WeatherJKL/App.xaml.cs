54cfe278fec6a2023cc5cd7fae9dc709921b7fd8
Umps <karl.pongolini@gmail.com>
2023-04-17 14:19:41 +0300

    Add project files.

---
+++ WeatherJKL/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherJKL;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WeatherJKL/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherJKL;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
